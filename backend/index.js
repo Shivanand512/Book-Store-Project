50946ccf6e9ca597e01cf4696fe2ce7b21f7e541
Biradar Shivanand <115731796+Shivanand512@users.noreply.github.com>
2024-10-10 11:31:26 +0530

    Updated backend to serve frontend in production

--- backend/index.js
+++ backend/index.js
@@ -1,58 +1,43 @@
-import  express from "express"
-import cors from 'cors'
-import {Book} from './models/bookModel.js'
-import {PORT,mongoDBURL} from './config.js';
-import mongoose from 'mongoose'
-import bookRouter from './routes/bookRoute.js'
-const app=express();
-import path from 'path';
-import dotenv from 'dotenv';
+import express from "express";
+import cors from "cors";
+import path from "path";
+import { Book } from "./models/bookModel.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import bookRouter from "./routes/bookRoute.js";
+import dotenv from "dotenv";
+
 dotenv.config();
 
+const app = express();
 app.use(cors());
-//2. allowa custom origins
-// app.use(
-//   cors({
-//     origin: "http://localhost:5000/",
-//     methods:['GET','POST','PUT','DELETE'],
-//     allowedHeaders:['Content-Type'],
-//   })
-// );
+app.use(express.json());
 
-app.use(express.json())
+app.use("/books", bookRouter);
 
-app.get('/',(req,res)=>{
-    res.send("Get Method")
-})
+const __dirname = path.resolve();
 
-app.use('/books',bookRouter);
-
-
-//-----------deployement
-
-const _dirname = path.resolve();
+// Serve frontend in production
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(_dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 } else {
   app.get("/", (req, res) => {
-    res.send("API running succefully");
+    res.send("API running successfully");
   });
 }
 
-
-
-
-mongoose.connect(mongoDBURL)
-.then(()=>{
-    console.log("App connected to database")
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("App connected to database");
     app.listen(PORT, () => {
-      console.log(`App is listening to PORT ${PORT}`);
+      console.log(`App is listening on PORT ${PORT}`);
     });
-})
-.catch((error)=>{
-    console.log(error)
-})
-
+  })
+  .catch((error) => {
+    console.log(error);
+  });
